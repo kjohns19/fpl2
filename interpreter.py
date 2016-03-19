3bcaad70887decc268eb7c8d8d0f5d29d594c005
Kevin Johnson <krjohnson111@gmail.com>
2016-03-18 21:36:58 -0400

    Basic tokenizing, added -c/--command argument

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,11 @@
+import tokenizer
+
 def run_file(filename):
-    print('Running: ' + filename)
-    #TODO
+    with open(filename, 'r') as f:
+        code = f.read()
+    return run_code(code)
+
+def run_code(code):
+    tokens = tokenizer.tokenize(code)
+    for token in tokens:
+        print(token)
