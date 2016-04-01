c4e9ef7f65606fdd9d2c20c355f3c49a5bb349da
Kevin Johnson <krjohnson111@gmail.com>
2016-04-01 09:05:31 -0400

    'then' changed to 'if ... then', added 'while ... do'

--- src/fpl/parser.py
+++ src/fpl/parser.py
@@ -12,37 +12,51 @@ class Parser:
         print(expanded)
         return expanded
 
-    def __parsethen(self, tokenization, index):
-        ifnode, index = self.__parse(tokenization, index=index, allowelse=True, allowend=True)
+    def __parse_if(self, tokenization, index):
+        condition, index = self.__parse(tokenization, index=index, stop=['then'])
+        ifnode, index = self.__parse(tokenization, index=index+1, stop=['else', 'end'])
         elsenode = None
         if tokenization[index].value == 'else':
-            elsenode, index = self.__parse(tokenization, index=index+1, allowelse=False, allowend=True)
-        if tokenization[index].value != 'end':
-            #TODO throw something here
-            print('ERROR: Expected \'end\'', file=sys.stderr)
-            sys.exit(1)
-        return fpl.syntax.NodeThen(ifnode, elsenode), index
+            elsenode, index = self.__parse(tokenization, index=index+1, stop=['end'])
+        return fpl.syntax.NodeThen(condition, ifnode, elsenode), index
+
+    def __parse_while(self, tokenization, index):
+        print('Parsing while')
+        condition, index = self.__parse(tokenization, index=index, stop=['do'])
+        whilenode, index = self.__parse(tokenization, index=index+1, stop=['end'])
+        print('Done parsing while')
+        return fpl.syntax.NodeWhile(condition, whilenode), index
         
-    def __parse(self, tokenization, index=0, allowelse=False, allowend=False):
+    def __parse(self, tokenization, index=0, stop=[]):
         block = fpl.syntax.Block()
         while index < len(tokenization):
             token = tokenization[index]
             node = None
 
             if type(token) is fpl.symbol.Symbol:
-                for value, allowed in [('else', allowelse), ('end', allowend)]:
-                    if token.value == value:
-                        if allowed:
-                            return block, index
-                        #TODO throw something here
-                        print('ERROR: Unexpected \'' + value + '\'', file=sys.stderr)
-                        sys.exit(1)
-                if token.value == 'then':
-                    node, index = self.__parsethen(tokenization, index=index+1)
+                if token.value in stop:
+                    return block, index
+                if token.value in ['then', 'do', 'else', 'end']: #TODO don't hardcode this?
+                    #TODO throw something here
+                    print('ERROR: Unexpected \'' + token.value + '\'', file=sys.stderr)
+                    print('Stop=' + str(stop))
+                    sys.exit(1)
+                if token.value == 'if':
+                    node, index = self.__parse_if(tokenization, index=index+1)
+                elif token.value == 'while':
+                    node, index = self.__parse_while(tokenization, index=index+1)
 
             if not node:
                 node = fpl.syntax.Node(token)
-
             block.add(node)
             index += 1
+
+        if stop:
+            #TODO throw something here
+            if len(stop) == 1:
+                msg = '\'' + stop[0] + '\''
+            else:
+                msg = 'one of (\'' + '\', \''.join(stop) + '\')'
+            print('ERROR: Expected ' + msg, file=sys.stderr)
+            sys.exit(1)
         return block, index
--- src/fpl/syntax.py
+++ src/fpl/syntax.py
@@ -21,11 +21,13 @@ class Block(Node):
 
 
 class NodeThen(Node):
-    def __init__(self, ifnode, elsenode=None):
+    def __init__(self, condition, ifnode, elsenode=None):
+        self.condition = condition
         self.ifnode = ifnode
         self.elsenode = elsenode
 
     def expand(self):
+        condition = self.condition.expand()
         ifcode = self.ifnode.expand()
         elsecode = self.elsenode.expand() if self.elsenode else []
         jmpamount = len(ifcode) + (2 if elsecode else 0)
@@ -33,7 +35,7 @@ class NodeThen(Node):
             fpl.number.Number(jmpamount),
             fpl.operator.Operator.get_operator('jmpnif')
         ]
-        code = jmpnif + ifcode
+        code = condition + jmpnif + ifcode
         if elsecode:
             jmp = [
                 fpl.number.Number(len(elsecode)),
@@ -41,3 +43,22 @@ class NodeThen(Node):
             ]
             code += jmp + elsecode
         return code
+
+class NodeWhile(Node):
+    def __init__(self, condition, whilenode):
+        self.condition = condition
+        self.whilenode = whilenode
+
+    def expand(self):
+        condition = self.condition.expand()
+        whilenode = self.whilenode.expand()
+        jmpnif = [
+            fpl.number.Number(len(whilenode)+2),
+            fpl.operator.Operator.get_operator('jmpnif')
+        ]
+        jmp = [
+            fpl.number.Number(-(len(condition) + len(whilenode) + 4)),
+            fpl.operator.Operator.get_operator('jmp')
+        ]
+        return condition + jmpnif + whilenode + jmp
+
